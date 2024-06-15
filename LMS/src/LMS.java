a988597e717d6561c9b266ad6ca0a1a5c6116ef8
Gerry <Glopez92@mail.valenciacollege.edu>
2024-06-15 12:27:06 -0400

    LMS Check-In and Check Out featurse implemented

--- LMS/src/LMS.java
+++ LMS/src/LMS.java
@@ -9,69 +9,99 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class LMS{
-    
-    
-    
+public class LMS {
 
     public static void main(String[] args) {
-        
+
         Scanner keyboard = new Scanner(System.in);
         char userChoice;
         Path file;
-        boolean isrunning = true; 
+        boolean isrunning = true;
         BookCollection collection = new BookCollection();
 
-        while(isrunning){  
+        while (isrunning) {
 
-            System.out.println("Please select a menu choice: A to add books, R to remove books, D to display books and Q to quit");
-            userChoice = keyboard.nextLine().charAt(0);
+            System.out.println(
+                    "Please select a menu choice: 'A' to add books, 'R' to remove books, 'D' to display books, 'O' to Check out a book, 'I' to check in a book and 'Q' to quit");
+            String userChoices = keyboard.nextLine();
+            while(userChoices.isEmpty()){
+                System.out.println(
+                    "Please select a menu choice: 'A' to add books, 'R' to remove books, 'D' to display books, 'O' to Check out a book, 'I' to check in a book and 'Q' to quit");
+             userChoices = keyboard.nextLine();
+            }
+            userChoice = userChoices.charAt(0);
             userChoice = Character.toUpperCase(userChoice);
-
-
-            switch(userChoice){
-            case 'A':
-            System.out.println("Please provide a file path for the text file. WARNING **File must be in .txt format and lines formatted correctly**");
-            String path = keyboard.nextLine();
-            file = Paths.get(path);
-            collection.addBookToCollection(file);
-            break;
             
-            case 'R':
-            System.out.println("Please provide a book id to remove from the collection. ");
-            String bookID = keyboard.nextLine();
 
-            collection.removeBooksFromCollection(bookID);
-            break;
+            switch (userChoice) {
+                case 'A':
+                    System.out.println(
+                            "Please provide a file path for the text file. WARNING **File must be in .txt format and lines formatted correctly**");
+                    String path = keyboard.nextLine();
+                    file = Paths.get(path);
+                    collection.addBookToCollection(file);
+                    break;
 
+                case 'R':
+                    char userRemoveChoice = ' ';
+                    System.out.println("Enter 'I' to remove book by ID number,'T' to remove by title or 'E to exit");
+                    userRemoveChoice = keyboard.nextLine().toUpperCase().charAt(0);
 
-            case 'D':
-            collection.displayBooks();
-            break;
-            
-            case 'Q':
-                System.out.println("Exiting the program. Have a great day.");
-                isrunning = false;
-            
-            default:
-                System.out.println("Please make a valid choice");
+                        switch(userRemoveChoice){
+                            case 'I':
+                                System.out.println("Please provide a book id to remove from the collection. ");
+                                String bookID = keyboard.nextLine();
+                                collection.removeBooksFromCollection(bookID);
+                                break;
+                            
+                            case 'T':
+                                System.out.println("Please provide a book title to remove from the collection. ");
+                                String bookTitle = keyboard.nextLine();
+                                collection.removeBookByTitle(bookTitle);
+                                break;
+                            
+                            case 'E':
+                                break;
+                            
+                            default:
+                                System.out.println("invalid choice");
+                                break;
+
+                 }
+                case 'D':
+                    collection.displayBooks();
+                    break;
+
+                case 'O':
+                String bookTitle;
+                System.out.println("Please provide the book title for check out");
+                bookTitle = keyboard.nextLine();
+                collection.checkOutBook(bookTitle);
+                collection.displayBooks();
                 break;
-                
 
-            
+                case 'I':
+                String bookCheckInTitle;
+                System.out.println("Please provide the book title for check-in");
+                bookCheckInTitle = keyboard.nextLine();
+                collection.checkInBook(bookCheckInTitle);
+                collection.displayBooks();
+                break;
+
+                case 'Q':
+                    System.out.println("Exiting the program. Have a great day.");
+                    isrunning = false;
+                    break;
+
+                default:
+                    System.out.println("Please make a valid choice");
+                    break;
+
+            }
+
         }
 
-
-
-
-       
-        }  
-
-
-       
         keyboard.close();
 
-      
     }
 }
-
